20d932781b302eb2683d9b7d2988811d69e56c1a
Lucas Rebuffo <62979002+LucasRebuffo@users.noreply.github.com>
2020-05-03 13:05:25 -0300

    TP rehecho

--- TP_1/TP_SSL.c
+++ TP_1/TP_SSL.c
@@ -9,166 +9,156 @@
 #define ESTADO4 4
 #define ESTADO5 5
 #define ERROR 6
+#define FILA 6
+#define COL 5
 
-
-char *concatenar(char *cadena, char caracter)
+int buscarEnTabla(int tabla[][COL] , int estado , int actualChar ) // busca en tabla el siguiente estado dado un estado actual y un caracter
 {
-   int i;
-
-   /* Encontrar el fin de cadena */
-   for (i = 0; cadena[i] != '\0'; i++)
-      ;
-
-   /* Añadimos el caracter */
-   cadena[i++] = caracter;
-
-   /* Añadimos el caracter de fin de cadena */
-   cadena[i] = '\0';
-
-   return cadena;
-}
-
-int main()
-{
-   int estado = INICIAL;
-
-   int actualChar = 0 ;
-
-   char buffer[100+1];
-
-   FILE* f = fopen( "entrada.txt" , "rb" );  // abro archivo a para leer
-
-   if(f == NULL)
+   int res;
+   for (int i = 0; i < COL; i++)
    {
-       perror("Error en la apertura de archivo");
-       return 1;
-
-   }
-
-   while( actualChar != EOF )  //lee hasta que sea final de archivo
-   {
-      actualChar = getc( f );  // tomo primer caracter del archivo
-
-      while( actualChar != ',' || feof(f) ) // itero hasta que salga ',' o se termine el archivo
-      {
-         concatenar( buffer , actualChar ); //concateno letra al final de buffer
-
-         switch (estado)  // me fijo el estado actual y lo llevo al estado siguiente
+      switch (tabla[estado][i])  // me fijo el estado actual y lo llevo al estado siguiente
          {
          case INICIAL:
             if (actualChar == '0')
             {
-               estado = ESTADO2;
+               res = ESTADO2;
             }
             else if (actualChar >= '1' && actualChar <= '9')
             {
-               estado = ESTADO1;
+               res = ESTADO1;
             }
             else
             {
-               estado = ERROR;
+               res = ERROR;
             }
             break;
 
          case ESTADO1:
             if (actualChar >= '0' && actualChar <= '9')
             {
-               estado = ESTADO1;
+               res = ESTADO1;
             }
             else
             {
-               estado = ERROR;
+               res = ERROR;
             }
             break;
 
          case ESTADO2:
             if (actualChar >= '0' && actualChar <= '7')
             {
-               estado = ESTADO3;
+               res = ESTADO3;
             }
             else if (actualChar == 'x'  || actualChar == 'X')
             {
-               estado = ESTADO4;
+               res = ESTADO4;
             }
             else
             {
-               estado = ERROR;
+               res = ERROR;
             }
             break;
 
          case ESTADO3:
             if(actualChar >= '0' && actualChar <= '7')
             {
-                estado = ESTADO3;
+                res = ESTADO3;
             }
             else
             {
-                estado =ERROR;
+                res =ERROR;
             }
 
          case ESTADO4:
             if ((actualChar >= '0' && actualChar <= '9') || (actualChar >= 'a' && actualChar <= 'f') || (actualChar >= 'A' && actualChar <= 'F'))
             {
-               estado = ESTADO5;
+               res = ESTADO5;
             }
             else
             {
-               estado = ERROR;
+               res = ERROR;
             }
             break;
 
          case ESTADO5:
             if ( (actualChar >= '0' && actualChar <= '9') ||(actualChar >= 'a' && actualChar <= 'f') || (actualChar >= 'A' && actualChar <= 'F'))
             {
-               estado = ESTADO5;
+               res = ESTADO5;
             }
             else
             {
-               estado = ERROR;
+               res = ERROR;
             }
             break;
 
          case ERROR:
 
+               res = ERROR;
             break;
+         
          }
+   }
+   return res;
+}
 
-         actualChar = getc( f );
-      }
+int main()
+{
+   int estado = INICIAL; 
+   int actualChar ;
 
-       //imprimo los resultados y vacio el buffer
-         if(estado == ERROR)
-         {
-            printf ("%s  no es una palabra válida" , buffer);
-            strcpy(buffer , "");
-         }
-         else if (estado == ESTADO1)
-         {
-            printf ("%s  es un numero decimal" , buffer);
-            strcpy(buffer , "");
-         }
-         else if (estado == ESTADO3)
-         {
-            printf ("%s  es un numero octal" , buffer);
-            strcpy(buffer , "");
-         }
-          else if (estado == ESTADO4)
-         {
-            printf ("%s  es un numero hexadecimal" , buffer);
-            strcpy(buffer , "");
-         }
+// llenar tabla
+   int tabla[FILA][COL] = {{ESTADO2,ESTADO1,ESTADO1,ERROR,ERROR} ,
+                     {ESTADO1,ESTADO1,ESTADO1,ERROR,ERROR} ,
+                     {ERROR,ESTADO3,ERROR,ESTADO4,ERROR} ,
+                     {ESTADO3,ESTADO3,ERROR,ERROR,ERROR} ,
+                     {ESTADO5,ESTADO5,ESTADO5,ERROR,ESTADO5} , 
+                     {ESTADO5,ESTADO5,ESTADO5,ERROR,ESTADO5}};
 
-        system("pause");
-
-         //vuelvo al estado inicial
-         estado = INICIAL;
-
-
-
+   FILE* f = fopen( "entrada.txt" , "rb" );  // abro archivo a para leer
+   FILE* w = fopen( "salida.txt" , "w");  //abro archivo para escribir
+   if(f == NULL)
+   {
+       perror("Error en la apertura de archivo");
+       return 1;
    }
 
-   fclose(f); //cierro el archivo
+   actualChar = getc(f); //leo un caracter
 
+   while( actualChar != EOF )  //lee hasta que sea final de archivo
+   {
+      while (actualChar != ',') //leo hasta que aparezca una coma
+      {
+         estado = buscarEnTabla(tabla , estado ,actualChar); //busco proximo estado
+
+         putc ( actualChar , w); //imprimo en archivo salida
+
+         actualChar = getc(f); // leo el siguiente
+      }
+      //imprimo el resultado
+      if(estado == ESTADO1)
+      {
+         fputs(" es decimal\n" , w );
+         
+      }
+      if(estado == ESTADO3)
+      {
+         fputs (" es octal\n" , w);
+      }
+      if(estado == ESTADO5)
+      {
+         fputs (" es hexadecimal\n" , w);
+      }
+      else
+      {
+         fputs (" no es reconocible\n" , w);
+      }
+
+      estado = INICIAL; // regreso al estado inicial
+      actualChar=getc(f); // leo la cadena siguiente
+   }
+   fclose(f); //cierro el archivo lectura
+   fclose(w); //cierro archivo salida
 
    return 0;
 }
